666f5701e7a94d4fa7892c64e57076b4bb209f92
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-02 14:52:57 +0530

    [Loveleen] ADD: JUnit testing

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java Unit Testing
+## JUnit
---
+++ src/test/java/com/bridgelabz/basicJUnit/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.basicJUnit;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Test
+    public void addTest() {
+        assertEquals(60, Calculator.add(15, 45));
+        assertEquals(0, Calculator.add(-5,5));
+    }
+
+    @Test
+    public void subtractTest() {
+        assertEquals(2, Calculator.subtract(6, 4));
+        assertEquals(-10, Calculator.subtract(-5, 5));
+    }
+
+    @Test
+    public void multiplyTest() {
+        assertEquals(675, Calculator.multiply(15, 45));
+        assertEquals(-25, Calculator.multiply(-5, 5));
+    }
+
+    @Test
+    public void divideTest() {
+        assertEquals(2, Calculator.divide(10, 5));
+    }
+
+    @Test
+    public void divideByZeroTest() {
+        assertThrows(ArithmeticException.class, () -> Calculator.divide(10, 0));
+    }
+}
